74553a17b330f50f282aaba311cd49bf32acadb2
Stathis Stamoulis <sstamoulis2010@gmail.com>
2023-02-07 13:13:04 +0200

    implement block iterator

--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -1,9 +1,8 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::sync::Arc;
 
 use super::Block;
+use crate::global_const::U16_SIZE;
+use crate::utils::FromLeBytesSlice;
 
 /// Iterates on a block.
 pub struct BlockIterator {
@@ -25,41 +24,64 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut it = Self::new(block);
+        it.next();
+        it
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: &[u8]) -> Self {
-        unimplemented!()
+        let mut it = Self::new(block);
+        it.seek_to_key(key);
+        it
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> &[u8] {
-        unimplemented!()
+        &self.key
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        &self.value
     }
 
     /// Returns true if the iterator is valid.
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.key.is_empty()
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        self.idx = 0;
+        self.next();
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        if let Some(offset) = self.block.offsets.get(self.idx) {
+            let data = &self.block.data[*offset as usize..];
+            let (key_len, data) = data.split_at(U16_SIZE);
+            let key_len = u16::from_le_bytes_slice(key_len) as usize;
+            let (key, data) = data.split_at(key_len);
+            self.key = key.into();
+            let (value_len, data) = data.split_at(U16_SIZE);
+            let value_len = u16::from_le_bytes_slice(value_len) as usize;
+            self.value = Vec::from(&data[..value_len]);
+            self.idx += 1;
+        } else {
+            // reached end
+            self.key = Vec::new();
+            self.value = Vec::new();
+        }
     }
 
     /// Seek to the first key that >= `key`.
     pub fn seek_to_key(&mut self, key: &[u8]) {
-        unimplemented!()
+        self.seek_to_first();
+        while self.is_valid() && self.key.as_slice() < key {
+            eprintln!("{}", std::str::from_utf8(self.key.as_slice()).unwrap());
+            self.next();
+        }
     }
 }
